4f889256aa788597d2158ead08bd575dbe9a2e98
fares ayman <149837282+fares527@users.noreply.github.com>
2024-12-16 20:21:46 +0200

    mm

---
+++ control-flow/daily_reminder.py
@@ -0,0 +1,22 @@
+task = input(" input a task description: ")
+priority_variable = input("the task’s priority (high, medium, low): ")
+time_bound = input(" time-bound (yes or no): ")
+
+
+match priority_variable:
+    case "high":
+      reminder = f"**Urgent!** {task}"
+      if time_bound == "yes":
+        reminder += " that requires immediate attention today!"
+    case "medium":
+      reminder = f"**Important:** {task}"
+      if time_bound == "yes":
+        reminder += " - Please address soon."
+    case "low":
+      reminder = f"{task}"
+      if time_bound == "yes":
+        reminder += " - Keep in mind."
+    case _:
+      reminder = f"**Unknown Priority:** {task}"
+
+print(reminder)
