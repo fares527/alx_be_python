9ca0371d5e4161f7b5e1104a672b3a2bb58ca9e9
fares ayman <149837282+fares527@users.noreply.github.com>
2024-12-16 14:20:19 +0200

    my first operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,12 @@
+number1=10
+number2=5
+
+Addition = number1+number2
+Subtraction = number2 - number2
+Multiplication = number1 * number2
+
+ #[operation] of [number1] and [number2] is [result].
+
+print( "Addition of " + str(number1) + " and " + str(number2) + " is " + str(Addition) )
+print( "Subtraction of " + str(number1) + " and " + str(number2) + " is " + str(Subtraction) )
+print( "Multiplication of " + str(number1) + " and " + str(number2) + " is " + str(Multiplication))
