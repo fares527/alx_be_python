c8fcf09ff48836d1bbb3886b8acfa9b8c081fdfe
fares ayman <149837282+fares527@users.noreply.github.com>
2024-12-20 15:22:09 +0200

    N ew

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,6 +1,7 @@
-Task = input(" Enter your task: ")
-Priority = input(" Priority (high/medium/low): ")
-Time_Bound = input(" Is it time-bound? (yes/no):  ")
+Task = input("Enter your task: ")
+Time_Bound = input("Is it time-bound? (yes/no):  ")
+Priority = input("Priority (high/medium/low): ")
+
 
 
 match Priority:
