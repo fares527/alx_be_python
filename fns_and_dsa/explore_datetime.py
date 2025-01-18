bb766867e605cdcf611c12392268241cb88323d2
fares ayman <149837282+fares527@users.noreply.github.com>
2025-01-18 16:10:02 +0200

    my third func

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -3,6 +3,7 @@ from datetime import timedelta
 
 def display_current_datetime():
     current_date = datetime.datetime.now()
+    current_date = current_date.strftime("%Y-%B-%D %I:%M:%P")
     print(current_date)
 
 
