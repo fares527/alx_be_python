7be2fab6c25d77c977c327549832b325db13cd69
fares ayman <149837282+fares527@users.noreply.github.com>
2025-01-18 16:16:06 +0200

    my third func

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -7,7 +7,7 @@ def display_current_datetime():
     print(current_date)
 
 
-number_of_days = input(int("Enter number of days: "))
+number_of_days = input(int("Enter the number of days to add to the current date: "))
 def calculate_future_date():
     display_current_datetime()
     future_date = current_date + timedelta(number_of_days)
