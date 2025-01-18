34f6d0d249a036481b8b18310f8f38ca123773aa
fares ayman <149837282+fares527@users.noreply.github.com>
2025-01-18 16:01:39 +0200

    my thired func

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,18 @@
+import datetime
+from datetime import timedelta
+
+def display_current_datetime():
+    current_date = datetime.datetime.now()
+    print(current_date)
+
+
+number_of_days = input(int("Enter number of days: "))
+def calculate_future_date():
+    display_current_datetime()
+    future_date = current_date + timedelta(number_of_days)
+    print(future_date)
+    
+
+
+if __name__ == "__main__":
+    calculate_future_date()
