f287dc6db09138e4cb05a97c45a53dd01bc11ed1
fares ayman <149837282+fares527@users.noreply.github.com>
2025-01-18 14:43:42 +0200

    my sec func

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -13,11 +13,11 @@ def main():
         choice = input(int("Enter your choice: "))
 
         if choice == '1':
-            item = input("Enter your item: ")
+            item = input("Enter the item to add: ")
             shopping_list.append(item)
             pass
         elif choice == '2':
-            item = input("Enter your item: ")
+            item = input("Enter the item to remove: ")
             shopping_list.remove(item)
             if item not in shopping_list:
                 print(f"{item} doesnt exist")
