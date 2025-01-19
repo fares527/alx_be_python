bf5db92086f9a1b2c4361e90f273e91066649515
fares ayman <149837282+fares527@users.noreply.github.com>
2025-01-19 21:02:52 +0200

    fs

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -3,13 +3,13 @@ def perform_operation(num1, num2, operation):
     num2 =float(input("Enter your second number: "))
     operation =input("Enter your operation (add, subtract, multiply, divide): ").lower()
 
-    if operation== add:
+    if operation== "add":
         print(num1 + num2)
-    elif operation== subtract:
+    elif operation== "subtract":
         print(num1 - num2)
-    elif operation== multiply:
+    elif operation== "multiply":
         print(num1 * num2)
-    elif operation== divide:
+    elif operation== "divide":
         if num2 == 0:
             return("cant divide by zero")
         print(num1 / num2)
