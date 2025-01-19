2cba0a4277263a24a8773c7748845c02bffd7bd2
fares ayman <149837282+fares527@users.noreply.github.com>
2025-01-19 16:44:31 +0200

    c

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,21 +1,21 @@
 class BankAccount:
     def __init__(self, account_balance=0.0):
-        self.account_balance=account_balance
+        self.current_balance=account_balance
        
     def deposit(self , amount):
         if amount <=0 :
             raise ValueError("account balance must be positive number")
-        self.account_balance += amount
+        self.current_balance += amount
 
     def withdraw(self, amount ):
          if amount <=0 :
             raise ValueError("account balance must be positive number")
-         if amount > self.account_balance :
+         if amount > self.current_balance :
              return False
-         self.account_balance -= amount
+         self.current_balance -= amount
 
     def display_balance(self):
-        print(f"The current balance = {self.account_balance: .2f} $ ") 
+        print(f"The current balance = {self.current_balance: .2f} $ ") 
          
         
         
