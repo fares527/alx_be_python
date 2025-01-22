20df768b6b907a4ee1d3dc3d0b4a8e86dbc3693a
fares ayman <149837282+fares527@users.noreply.github.com>
2025-01-22 14:36:42 +0200

    l

--- oop/library_system.py
+++ oop/library_system.py
@@ -24,14 +24,14 @@ class PrintBook(Book):
 
 class Library:
     def __init__(self):
-         self.books= []
+         self.books = []
        
     def add_book(self, book):
         self.books.append(book)
     
     def list_books(self):
         for book in self.books:
-          print(book)
+          print(book) 
        
 
         
